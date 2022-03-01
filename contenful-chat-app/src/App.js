77e181e1866042a7d6e3d6fc8661ca0468a89500
uche onuekwusi <ucheonuekwusi1234@gmail.com>
2022-03-01 18:53:13 +0000

    creation of chat feed

--- contenful-chat-app/src/App.js
+++ contenful-chat-app/src/App.js
@@ -1,2 +1,17 @@
 import { ChatEngine } from "react-chat-engine";
+import ChatFeed from "./components/ChatFeed";
 import "./App.css";
+
+const App = () => {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="c2001816-f072-4ca4-b053-0fcd91665a46"
+      userName="uche"
+      userSecret="1234567"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
+  );
+};
+
+export default App;
